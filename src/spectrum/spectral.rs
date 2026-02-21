332a7d8f4e51610a9b9ee65c8af4c7bf5f7ed8e5
kaj <notkaj@gmail.com>
2026-02-20 19:24:54 -0500

    feat: added capture max to input popup

--- src/spectrum/spectral.rs
+++ src/spectrum/spectral.rs
@@ -1,3 +1,5 @@
+use crate::capture::capturer::capture;
+
 pub trait Spectral {
     // fn init(&mut self);
     fn update(&mut self);
@@ -13,3 +15,11 @@ pub(super) const DEFAULT_RANGE_COUNT: usize = 36;
 pub(super) const RATIO: f32 = 0.10;
 pub(super) const OFFSET: usize = 0;
 pub(super) const DEFAULT_SCALE: f32 = 24.0;
+
+pub(crate) fn max_cap() -> Option<f32> {
+    capture()
+        .unwrap_or_default()
+        .iter()
+        .max_by(|a, b| a.total_cmp(b))
+        .copied()
+}
--- src/ui/popup/input.rs
+++ src/ui/popup/input.rs
@@ -6,7 +6,7 @@ use ratatui::{
     widgets::{Block, BorderType, Clear, Widget},
 };
 
-use crate::ui::popup::HORIZ_PERCENT;
+use crate::{spectrum::spectral::max_cap, ui::popup::HORIZ_PERCENT};
 use tui_barchart_ext::barchart::{Bar, BarChart};
 
 pub struct InputPopup {
@@ -28,7 +28,7 @@ impl InputPopup {
 }
 impl Widget for &InputPopup {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let vertical = Layout::vertical([Constraint::Length(4)]).flex(Flex::Center);
+        let vertical = Layout::vertical([Constraint::Length(6)]).flex(Flex::Center);
         let horizontal =
             Layout::horizontal([Constraint::Percentage(HORIZ_PERCENT)]).flex(Flex::Center);
         let [area] = vertical.areas(area);
@@ -40,24 +40,39 @@ impl Widget for &InputPopup {
             .border_type(BorderType::Rounded);
 
         let inner = b.inner(area);
-        let text = Text::from(vec![Line::from(format!(
-            "Channels: {} | Rate: {}",
-            self.channels, self.rate
-        ))]);
+        let text = Text::from(vec![
+            Line::from(format!("Channels: {} | Rate: {}", self.channels, self.rate)),
+            Line::from("Maximums:"),
+        ]);
         let width = inner.width;
-        let chart = BarChart::horizontal(vec![
+        let chart_max_amp = BarChart::horizontal(vec![
             Bar::default()
-                .label(format!("{:<3} ", self.max))
-                .text_value("")
+                .text_value(format!("{:<3} ", self.max))
+                .label("amp")
                 .value(self.max as u64),
         ])
         .max((width - 3) as u64 * 8);
-        let vert = Layout::vertical([Constraint::Length(1), Constraint::Fill(1)]);
-        let [stats, max] = vert.areas::<2>(inner);
+        let vert = Layout::vertical([
+            Constraint::Length(2),
+            Constraint::Length(1),
+            Constraint::Length(1),
+        ]);
+
+        let max_cap = (max_cap().unwrap_or_default() * 1000.0) as u64;
+        let chart_max_cap = BarChart::horizontal(vec![
+            Bar::default()
+                .text_value(format!("{:<3} ", max_cap))
+                .label("cap")
+                .value(max_cap as u64),
+        ])
+        .max((width - 3) as u64 * 8);
+
+        let [stats, amp, cap] = vert.areas::<3>(inner);
 
         Clear.render(area, buf); // cool effect if you comment this line out
         b.render(area, buf);
         text.render(stats, buf);
-        chart.render(max, buf);
+        chart_max_amp.render(amp, buf);
+        chart_max_cap.render(cap, buf);
     }
 }
