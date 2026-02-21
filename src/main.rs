bd22d82ca4aeeac15d380a66c9b8ecddfacc9ed7
kaj <notkaj@gmail.com>
2026-02-21 12:17:31 -0500

    feat: added cli argument for stereo

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use crate::{
     app::App,
     visualize::{mono::MonoVisualizer, stereo::StereoVisualizer},
 };
+use clap::Parser;
 
 mod app;
 mod capture;
@@ -16,7 +17,19 @@ mod visualize;
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     let terminal = ratatui::init();
-    let result = App::<StereoVisualizer>::new().run(terminal).await;
+    let args = Args::parse();
+    let is_stereo = args.stereo;
+    let result = if is_stereo {
+        App::<StereoVisualizer>::new().run(terminal).await
+    } else {
+        App::<MonoVisualizer>::new().run(terminal).await
+    };
     ratatui::restore();
     result
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    stereo: bool,
 }
