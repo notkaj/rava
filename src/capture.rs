bdd0e83bd8b343594635cb6d0bb907923028642f
kaj <notkaj@gmail.com>
2026-02-27 00:29:03 -0500

    refactor: moved max_cap to proper file

--- src/capture.rs
+++ src/capture.rs
@@ -3,3 +3,11 @@ pub mod pipewire;
 
 pub const DEFAULT_QUANT: usize = 2048;
 pub const DEFAULT_RATE: usize = 48000;
+
+pub(crate) fn max_cap() -> Option<f32> {
+    capturer::capture()
+        .unwrap_or_default()
+        .iter()
+        .max_by(|a, b| a.total_cmp(b))
+        .copied()
+}
--- src/spectrum.rs
+++ src/spectrum.rs
@@ -6,12 +6,3 @@ pub(super) const DEFAULT_RANGE_COUNT: usize = 36;
 pub(super) const RATIO: f32 = 0.06;
 pub(super) const OFFSET: usize = 0;
 pub(super) const DEFAULT_SCALE: f32 = 24.0;
-
-use crate::capture::capturer::capture;
-pub(crate) fn max_cap() -> Option<f32> {
-    capture()
-        .unwrap_or_default()
-        .iter()
-        .max_by(|a, b| a.total_cmp(b))
-        .copied()
-}
