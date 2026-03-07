f1bf8ee3a2dba471551090d7350f45b0a8ec12a8
kaj <notkaj@gmail.com>
2026-03-07 16:21:54 -0500

    refactor: minor review and adjustments to CavaFilter

--- src/filter.rs
+++ src/filter.rs
@@ -53,6 +53,7 @@ pub struct CavaFilter {
     noise_reduct: f32,
     prev_amps: Vec<f32>,
     post_int_vals: Vec<f32>,
+    gravity_mod: f32,
 }
 
 impl CavaFilter {
@@ -61,12 +62,14 @@ impl CavaFilter {
         let fall_vals = vec![0.0; len];
         let prev_amps = vec![0.0; len];
         let post_int_vals = vec![0.0; len];
+        let gravity_mod = ((60.0 / TICK_FPS as f32).powf(2.5) * 1.54 / noise_reduct).max(1.0);
         Self {
             curr_peaks,
             fall_vals,
             noise_reduct,
             prev_amps,
             post_int_vals,
+            gravity_mod,
         }
     }
 
@@ -88,14 +91,13 @@ impl Filter for CavaFilter {
         if self.curr_peaks.len() != len {
             self.resize(len)
         }
-        // let overshoot = 0;
-        let gravity_mod = ((60.0 / TICK_FPS as f32).powf(2.5) * 1.54 / self.noise_reduct).max(1.0);
 
         for (i, &amp) in input.iter().enumerate() {
             let prev = self.prev_amps[i];
             let mut res = amp;
             if amp < prev && self.noise_reduct > 0.1 {
-                res = self.curr_peaks[i] * (1.0 - (self.fall_vals[i].powi(2) * gravity_mod));
+                res = self.curr_peaks[i] * (1.0 - (self.fall_vals[i].powi(2) * self.gravity_mod));
+                res = res.max(0.0);
                 self.fall_vals[i] -= 0.028;
             } else {
                 self.curr_peaks[i] = amp;
@@ -104,9 +106,8 @@ impl Filter for CavaFilter {
             self.prev_amps[i] = res;
 
             // process integral smoothing
-            let post_int = self.post_int_vals[i] * self.noise_reduct + res;
-            self.post_int_vals[i] = post_int;
-            out[i] = post_int as u32;
+            self.post_int_vals[i] = self.post_int_vals[i] * self.noise_reduct + res;
+            out[i] = self.post_int_vals[i] as u32;
         }
     }
 }
