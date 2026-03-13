9d64de7b955f69d761eaacee636e17a7070b7c4d
kaj <notkaj@gmail.com>
2026-03-13 16:37:40 -0400

    refactor: split up some functions, preparing margin/auto-bars/auto-width

--- src/ui/mono.rs
+++ src/ui/mono.rs
@@ -13,32 +13,47 @@ use tui_barchart_ext::barchart::{Bar, BarChart};
 impl WidgetRef for MonoVisualizer {
     fn render_ref(&self, area: Rect, buf: &mut Buffer) {
         //TODO: maybe only recalculate this on terminal resize?
-        let bars = self.bars() as u16;
-        let width = area.width as u64;
-        let height = area.height as u64;
 
-        let chart = match (self.direction, self.orientation) {
-            (Direction::Vertical, Orientation::Inverted) => {
-                let bar_width = width as u16 / (bars + 1);
-                vertical_barchart(self, bar_width, height).inverted()
-            }
-            (Direction::Horizontal, Orientation::Inverted) => {
-                let bar_width = height as u16 / (bars + 1);
-                horizontal_barchart(self, bar_width, width).inverted()
-            }
-            (Direction::Vertical, _) => {
-                let bar_width = width as u16 / (bars + 1);
-                vertical_barchart(self, bar_width, height)
-            }
-            (Direction::Horizontal, _) => {
-                let bar_width = height as u16 / (bars + 1);
-                horizontal_barchart(self, bar_width, width)
-            }
+        match self.direction {
+            Direction::Vertical => render_vert(self, area, buf),
+            Direction::Horizontal => render_horiz(self, area, buf),
         };
 
-        chart.render(area, buf);
         Popup::render(self, &self.mode, area, buf);
     }
+}
+
+fn render_vert(vis: &MonoVisualizer, area: Rect, buf: &mut Buffer) {
+    let bars = vis.bars() as u16;
+    let width = area.width;
+    let height = area.height as u64;
+
+    // bars * (bar_width + 1) = width + 1
+    // bar_width + 1 = (width + 1)/bars
+    // bar_width = ((width + 1)/bars) - 1
+    let bar_width = ((width + 1) / bars) - 1;
+
+    let chart = match vis.orientation {
+        Orientation::Inverted => vertical_barchart(vis, bar_width, height).inverted(),
+        _ => vertical_barchart(vis, bar_width, height),
+    };
+
+    chart.render(area, buf);
+}
+
+fn render_horiz(vis: &MonoVisualizer, area: Rect, buf: &mut Buffer) {
+    let bars = vis.bars() as u16;
+    let width = area.width as u64;
+    let height = area.height;
+
+    let bar_width = ((height + 1) / bars) - 1;
+
+    let chart = match vis.orientation {
+        Orientation::Inverted => horizontal_barchart(vis, bar_width, width).inverted(),
+        _ => horizontal_barchart(vis, bar_width, width),
+    };
+
+    chart.render(area, buf);
 }
 
 fn horizontal_barchart(vis: &MonoVisualizer, bar_width: u16, width: u64) -> BarChart<'static> {
